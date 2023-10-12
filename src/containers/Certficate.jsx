fd5cbc2584f7cb50ef30a297081de9e2dbb1c806
karimliaghabala <agabala.93@gmail.com>
2023-10-12 23:22:25 +0400

    update

--- src/containers/Certficate.jsx
+++ src/containers/Certficate.jsx
@@ -98,7 +98,7 @@ const Certficate = () => {
       </div>
       <div className="course-info w-full  flex flex-col md:flex-row justify-between gap-6 items-center mt-8 p-2 border border-[rgba(255,255,255,0.1)] backdrop-blur-sm ">
       <div className="left-info text-white w-full flex flex-col items-center justify-around ">
-          <p className="text-header pt-4">{t('year4')}</p>
+          <p className="text-header pt-4 text-center" >{t('year4')}</p>
           <p className="text-lg pt-4 text-center">{t('edu4')}</p>
           <p className="text-base pt-4">{t('specialty4')}</p>
         </div>
